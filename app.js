f08e1b8d6930149ac02225c9dff4574886b48e51
Stan S <stan.shm@gmail.com>
2022-01-25 14:03:38 -0600

    predeployment prep/fix

--- app.js
+++ app.js
@@ -44,13 +44,13 @@ app.use('/projects', projectsRouter);
 app.use('/tickets', ticketsRouter);
 
 
-/* if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('bt_frontEnd','build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'bt_frontEnd','build', 'index.html'));
   });
 }
- */
+
 
 
 // catch 404 and forward to error handler
