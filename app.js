15fb1911fdaa5d0411d68619d1ed69302194d96a
Stan S <stan.shm@gmail.com>
2022-01-25 16:28:05 -0600

    index route removed

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
 app.use('/tickets', ticketsRouter);
