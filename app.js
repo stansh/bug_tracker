8ac8341d59bb5fe215b0b0cfaf744de875250d18
Stan S <stan.shm@gmail.com>
2022-12-14 00:51:16 -0600

    static files path updated

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/tickets', ticketsRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client'));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
   });
