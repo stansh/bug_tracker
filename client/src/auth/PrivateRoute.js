a971e7dcd1c30a8cf6d1c00c00e004c8a9cbf9e1
Stan S <stan.shm@gmail.com>
2022-12-14 22:53:11 -0600

    client removed

--- client/src/auth/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useUser } from './useUser';
-
-export const PrivateRoute = ({children})=> {
-    const user = useUser();
-    return user ? children : <Navigate to="/login" />;
-}
