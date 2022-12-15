7f415b9b00d2c13e33412a140e8ffa4701f668fd
Stan S <stan.shm@gmail.com>
2022-12-14 22:19:52 -0600

    index file path changed

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ app.use('/tickets', ticketsRouter);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
   })
 }
 
