517f8f2dfe9a4d5c8d5fa9610f35bfd1eac168be
Stan S <stan.shm@gmail.com>
2022-12-15 19:57:18 -0600

    change

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/tickets', ticketsRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'build','index.html'));
   })
 }
