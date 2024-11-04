474943772bb68fbdeff045bcce2cbb96083811aa
Stas <stan.shm@gmail.com>
2024-11-03 21:21:44 -0600

    changes to depoy to Azure

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 //const config = require('./config');
-const url = process.env.URL 
+// const url = process.env.URL 
 
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var projectsRouter = require('./routes/projects');
 var ticketsRouter = require('./routes/tickets');
 
 //const url = config.mongoUrl;
-const connect = mongoose.connect(process.env.MONGODB_URL || url, {
+const connect = mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 });
@@ -34,19 +36,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(cors({
+  origin: ['https://bugtrackerstas.azurewebsites.net','http://localhost:3000'],  
+  methods: ['GET', 'POST', 'PUT', 'DELETE']  
+}));
+
+// app.use(express.static(path.join(__dirname, '/build')));
 
 app.use(passport.initialize());
 
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
 app.use('/tickets', ticketsRouter);
 
  
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,'build','index.html'));
   })
